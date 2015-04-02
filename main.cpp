191bfbc1791d7882ad68e4a0d8c5d51e60ab9055
precisef0x <admin@onescript.ru>
2015-04-02 14:08:12 +0300

    main.cpp updated

--- main.cpp
+++ main.cpp
@@ -18,10 +18,10 @@ int main(void)
     
     Library *lib = new Library("My favorite library", "Right around the corner");
     lib->printLibraryInfo();
-    lib->addBook(book);
+    lib->add(book);
     book.setTitle("The Mystery of Cloomber");
     book.setAvailable(false);
-    lib->addBook(book);
+    lib->add(book);
     cout << endl;
     
     //Check whether the books counter changed or not
@@ -34,7 +34,7 @@ int main(void)
     //Create a new library from the first and add a new book
     Library lib2(*lib);
     Book *book2 = new Book("Cool title", "A genius", 2015, 0.12, 200, 1);
-    lib2.addBook(*book2);
+    lib2.add(*book2);
     
     cout <<endl << "New library: " << endl;
     lib2.printLibraryInfo();
@@ -50,7 +50,7 @@ int main(void)
     lib3.printBooks();
     
     //Delete the book from the third library:
-    lib3.deleteBook(*book2);
+    lib3.remove(*book2);
     
     //Print all books again
     cout << endl;
